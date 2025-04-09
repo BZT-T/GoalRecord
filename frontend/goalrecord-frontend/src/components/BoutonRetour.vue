09df434899a8baebe75b8c12965f76b0794d8d64
BZT-T <yassinebouazzati2@gmail.com>
2025-04-09 10:39:50 +0200

    Bouton responsiv

--- frontend/goalrecord-frontend/src/components/BoutonRetour.vue
+++ frontend/goalrecord-frontend/src/components/BoutonRetour.vue
@@ -1,5 +1,7 @@
 <template>
- <button class="button-53 bg-[#006400] border-0 text-white flex justify-center items-center font-sans text-[1rem] font-bold leading-[1.75rem] p-[0.75rem_1.65rem] relative text-center text-black border-b-2 max-w-[460px] w-full cursor-pointer transform rotate-[-2deg] select-none -webkit-select-none touch-manipulation m-2.5 focus:outline-none hover:after:border-white hover:after:bottom-[2px] hover:after:left-[2px] md:p-[0.75rem_3rem] md:text-[1.25rem]" @click="goBack" role="button">Retour</button>
+  <button class="button-53 bg-[#006400] border-0 text-white flex justify-center items-center font-sans text-[1rem] font-bold leading-[1.75rem] p-[0.75rem_1.65rem] relative text-center text-black border-b-2 max-w-[160px] lg:max-w-[460px] w-full cursor-pointer transform rotate-[-2deg] select-none -webkit-select-none touch-manipulation m-2.5 focus:outline-none hover:after:border-white hover:after:bottom-[2px] hover:after:left-[2px] md:p-[0.75rem_3rem] md:text-[1.25rem] sm:text-[0.875rem] sm:p-[0.5rem_1rem]" @click="goBack" role="button">
+    Retour
+  </button>
 </template>
 
 <script>
